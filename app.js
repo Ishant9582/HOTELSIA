6ce6df0bd87cf2fc6bac16ed589a678b321bd878
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-08-05 00:09:41 +0530

    aa

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ main()
 })
 .catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(process.env.ATLAS_URI);
 }
 const sessionOptions = {
     secret : "supersecret" , 
