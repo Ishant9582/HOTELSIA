1ae4e19992a58abc302fb26402f67d71dd92fd5f
Ishant9582 <150500780+Ishant9582@users.noreply.github.com>
2024-08-06 01:03:54 +0530

    oo

--- app.js
+++ app.js
@@ -37,25 +37,26 @@ main()
 async function main() {
   await mongoose.connect(process.env.ATLAS_URI);
 }
-// const sessionOptions = {
-//     secret : "yoursecretkey" , 
-//     // AGR NICHE VALE 2 USE NHI KIE TO WARNING AAYEGI
-//     resave : "false" , 
-//     saveUninitialized : true ,
-//     store : MongoStore.create({mongUrl : "mongodb+srv://ishant135:6S5BWVgGeAob7JtA@hotelsia.uox0mi8.mongodb.net/wanderlust"}) ,
-//     cookie : {
-//         expires : Date.now() + 7*24*60*60*1000 ,
-//         maxAge : 7*24*60*60*1000 ,
-//         httponly : true ,
-//     }
-// }
-// app.use(session(sessionOptions));
-app.use(session({
-    secret: 'yourSecretKey',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: 'mongodb+srv://ishant135:6S5BWVgGeAob7JtA@hotelsia.uox0mi8.mongodb.net/wanderlust'})
-}));
+
+const sessionOptions = {
+    store ,
+    secret : "yoursecretkey" , 
+    // AGR NICHE VALE 2 USE NHI KIE TO WARNING AAYEGI
+    resave : "false" , 
+    saveUninitialized : true ,
+    store : MongoStore.create({
+        mongUrl : "mongodb+srv://ishant135:6S5BWVgGeAob7JtA@hotelsia.uox0mi8.mongodb.net/wanderlust" ,
+        touchAfter : 24*3600 ,
+
+     }) ,
+    cookie : {
+        expires : Date.now() + 7*24*60*60*1000 ,
+        maxAge : 7*24*60*60*1000 ,
+        httponly : true ,
+    }
+}
+app.use(session(sessionOptions));
+
 app.use(flash()) ;
 
 // initializing passport
